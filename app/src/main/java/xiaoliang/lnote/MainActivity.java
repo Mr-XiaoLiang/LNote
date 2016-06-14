2a8c51b0e416d7b5841f4686bae46fa2ed0c47f2
XiaoLiang <liujian594@hotmail.com>
2016-06-14 08:31:11 +0800

    每日例行更新

--- app/src/main/java/xiaoliang/lnote/MainActivity.java
+++ app/src/main/java/xiaoliang/lnote/MainActivity.java
@@ -1,13 +1,43 @@
 package xiaoliang.lnote;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme_NoActionBar);
         setContentView(R.layout.activity_main);
+        toHome();
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 200:
+                    startActivity(new Intent(MainActivity.this,HomeActivity.class));
+                    finish();
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+
+    private void toHome(){
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.sendEmptyMessage(200);
+            }
+        }, 2000);//2秒后跳转
     }
 }
